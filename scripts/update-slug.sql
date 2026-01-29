9a89386477795ddef54c3cb4f1c8def23d97c1d6
April Lynn Gambardella <aprilgambardella@Aprils-MBP.lan>
2026-01-29 04:28:42 -0600

    Fix: Remove contentTextStyles from SQL update - uses contentId reference

--- scripts/update-slug.sql
+++ scripts/update-slug.sql
@@ -1,4 +1,3 @@
 UPDATE siteContent SET page='emerge-with-us' WHERE page='walk-with-us';
-UPDATE contentTextStyles SET page='emerge-with-us' WHERE page='walk-with-us';
 UPDATE pages SET slug='emerge-with-us', title='Emerge With Us' WHERE slug='walk-with-us';
 UPDATE navigation SET url='/emerge-with-us' WHERE url='/walk-with-us';
