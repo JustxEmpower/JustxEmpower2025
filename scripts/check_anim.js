d87cd5c6935e62a9b3052ed04949723119f5b3a0
April Lynn Gambardella <aprilgambardella@Aprils-MBP.lan>
2026-02-06 01:12:07 -0600

    fix: ESM import in check script

--- scripts/check_anim.js
+++ scripts/check_anim.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2/promise');
+import mysql from 'mysql2/promise';
 (async () => {
   const c = await mysql.createConnection('mysql://justxempower:JustEmpower2025Secure@justxempower-mysql.cg7k02qmmmt6.us-east-1.rds.amazonaws.com:3306/justxempower');
   const [rows] = await c.query('SELECT id, content FROM pageBlocks WHERE pageId=67');
